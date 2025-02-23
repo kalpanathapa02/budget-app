9effbe589e243bfe8e9da815b2f7a4894aba9c53
Kalpana Thapa <calpanathapa2058@gmail.com>
2025-02-23 13:49:15 -0500

    change the scope of the sw

--- assets/js/signIn.js
+++ assets/js/signIn.js
@@ -3,33 +3,43 @@ import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const provider = new GoogleAuthProvider();
 
-const signInBttn = document.getElementById('signIn');
+const signInBttn = document.getElementById("signIn");
 
-const sw = new URL('service-worker.js', import.meta.url);
+const sw = new URL("service-worker.js", import.meta.url);
 
-if ('serviceWorker' in navigator) {
-    const s = navigator.serviceWorker;
-    s.register(sw.href, {
-        scope: '/CheckList/'
-    }).then(_ => console.log('Service Worker Registered for scope:', sw.href, 'with', import.meta.url))
-        .catch(err => console.error('Service Worker Error:', err));
+if ("serviceWorker" in navigator) {
+  const s = navigator.serviceWorker;
+  s.register(sw.href, {
+    scope: "/budget-app/",
+  })
+    .then((_) =>
+      console.log(
+        "Service Worker Registered for scope:",
+        sw.href,
+        "with",
+        import.meta.url
+      )
+    )
+    .catch((err) => console.error("Service Worker Error:", err));
 }
 
 function signIn() {
-    signInWithPopup(auth, provider).then((result) => {
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        const user = result.user;
-        localStorage.setItem("email", JSON.stringify(user.email));
-        window.location = "tasks.html";
-    }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        const email = error.customData.email;
-        const credential = GoogleAuthProvider.credentialFromError(error);
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      localStorage.setItem("email", JSON.stringify(user.email));
+      window.location = "tasks.html";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
     });
 }
 
-signInBttn.addEventListener("click", function(event) {
-    signIn(auth, provider);
+signInBttn.addEventListener("click", function (event) {
+  signIn(auth, provider);
 });
